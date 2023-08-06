fa08dc49c3eca32a643ed7414680b03f45741ee7
AlphaCeph-C3 <101421534+AlphaCeph-C3@users.noreply.github.com>
2023-08-06 16:19:50 +0530

    edited for production

--- shareme_frontend/src/App.jsx
+++ shareme_frontend/src/App.jsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './container/Home';
+import { fetchUser } from './utils/fetchUser';
 
-const App = () => (
-  <Routes>
-    <Route path="login" element={<Login />} />
-    <Route path="/*" element={<Home />} />
-  </Routes>
-);
+const App = () => {
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    const user = fetchUser();
+
+    if (!user) {
+      navigate('/login');
+    }
+  }, []);
+
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
+};
 
 export default App;
